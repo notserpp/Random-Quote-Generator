9e8bf687e229fa651ac2e9de924a54253a0acb56
notserpp <prestonsegura@yahoo.com>
2018-12-25 17:22:11 -0800

    updated http to https to prevent mixed content

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
     function getQuote() {
         $.ajax({
-            url: 'http://api.forismatic.com/api/1.0/',
+            url: 'https://api.forismatic.com/api/1.0/',
             jsonp: 'jsonp',
             dataType: 'jsonp',
             data:{
